7d9fafd5667883759088483e8cfcc055c0b172d1
benjaminreadshaw <u5798145@anu.edu.au>
2018-04-05 06:02:00 +1000

    change stuff

--- main.py
+++ main.py
@@ -1,42 +1,93 @@
 import random, sys
 
-SPINS = 10000
-STREAK_SIZE = 5
+SPINS = 10
+STREAK_SIZE = 3
+NUM_ZEROS = 1
+PROPORTION_OF_ZEROES_IN_STREAK = 0.5
 
-def spin():
-	r = random.randint(0,36)
-	if r % 2 == 1:
-		return True
-	return False
+def single_spin():
+	r = random.randint(1,36 + max([1, NUM_ZEROS]))
+	if r > 36:
+		return 'zero'
+	if r % 2 == 0:
+		return 'even'
+	else:
+		return 'odd'
+
+def set_of_spins(n):
+	spins = []
+	for i in range(n):
+		spins.append(single_spin())
+	return spins
+
+def parse_spins(spins):
+	
+	total = 0
+
+	lastResult = 'zero'
+
+	results = {
+		'total': 0,
+		'odd': 0,
+		'even': 0}
+
+	spinsSinceCounters = {
+		'odd': 0,
+		'even': 0,
+		'zero': 0}
+
+	def did_bet(outcome):
+		more_than_streak_since = spinsSinceCounters[outcome] >= STREAK_SIZE
+		return more_than_streak_since
+
+
+
+	for result in spins:
+
+		lastResult = result
+
+		if result == "odd":
+			if did_bet('odd'):
+				results['total'] += 1	
+				results['odd'] += 1
+			elif did_bet('even'):
+				results['total'] += 1
+		
+			spinsSinceCounters['odd'] = 0
+			spinsSinceCounters['even'] += 1
+
+
+		elif result == "even":
+			if did_bet('even'):
+				results['total'] += 1	
+				results['even'] += 1
+			elif did_bet('odd'):
+				results['total'] += 1
+				
+			spinsSinceCounters['odd'] += 1
+			spinsSinceCounters['even'] = 0
+
+
+		elif result == 'zero':
+			if did_bet('even') or did_bet('odd'):
+				results['total'] += 1	
+				
+			spinsSinceCounters['odd'] += 1
+			spinsSinceCounters['even'] += 1
+
+
+		if SPINS <= 50:
+			print("result: {}\tsince odd: {}\teven: {}\tzero: {}\ttotal: {}\tsuccessful: {}".format(result, spinsSinceCounters['odd'], spinsSinceCounters['even'], spinsSinceCounters['zero'], results['total'], results['odd'] + results['even']))
+
+	return results
 
 def main():
 	
-	remaining = SPINS
-	spins = []
-	while remaining > 0:
-		spins.append(spin())
-		remaining -= 1
+	spins = set_of_spins(SPINS)
 
-	total = 0
-	successful = 0
+	results = parse_spins(spins)
 
-	lastResult = True
-	consecutiveFalse = 0
-	for result in spins:
-		lastResult = result
-		if result:
-			if consecutiveFalse >= 4:
-				total += 1
-				successful += 1
-			consecutiveFalse = 0
-		else:
-			if consecutiveFalse >= STREAK_SIZE:
-				total += 1
-			consecutiveFalse += 1
-		if SPINS <= 50:
-			print("result: {}\tconsecutive: {}\ttotal: {}\tsuccessful: {}".format(result, consecutiveFalse, total, successful))
-
-	print("Spun {} times\n{} Ouf of {} bets were successful\t{}% success rate".format(SPINS, successful, total, 100*successful/total))
+	print("odd: {}\teven {}\ttotal: {}\t{}".format(results['odd'], results['even'], results['total'], 100 *(results['odd'] + results['even'] )/ max([1,results['total']])))
 
 
 
